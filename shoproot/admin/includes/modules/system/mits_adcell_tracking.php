351536a9965a1bc62441e91d7b3381c0362004f8
hetfield74 <hetfield@merz-it-service.de>
2025-05-14 12:04:03 +0200

    some changes

--- shoproot/admin/includes/modules/system/mits_adcell_tracking.php
+++ shoproot/admin/includes/modules/system/mits_adcell_tracking.php
@@ -17,69 +17,100 @@
 
 defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
 
-class mits_adcell_tracking {
-  var $code, $title, $description, $enabled;
+class mits_adcell_tracking
+{
+    public $code;
+    public $name;
+    public $version;
+    public $title;
+    public $description;
+    public $sort_order;
+    public $enabled;
+    private $_check;
 
-  function __construct() {
-    $this->code = 'mits_adcell_tracking';
-    $this->name = 'MODULE_' . strtoupper($this->code);
-    $this->version = '1.01';
-    $this->title = constant($this->name . '_TITLE') . ' - v' . $this->version;
-    $this->description = constant($this->name . '_DESCRIPTION');
-    $this->sort_order = defined($this->name . '_SORT_ORDER') ? constant($this->name . '_SORT_ORDER') : 0;
-    $this->enabled = defined($this->name . '_STATUS') && constant($this->name . '_STATUS') == 'true' ? true : false;
+    function __construct()
+    {
+        $this->code = 'mits_adcell_tracking';
+        $this->name = 'MODULE_' . strtoupper($this->code);
+        $this->version = '1.02';
+        $this->title = constant($this->name . '_TITLE') . ' - v' . $this->version;
+        $this->description = constant($this->name . '_DESCRIPTION');
+        $this->sort_order = defined($this->name . '_SORT_ORDER') ? constant($this->name . '_SORT_ORDER') : 0;
+        $this->enabled = defined($this->name . '_STATUS') && constant($this->name . '_STATUS') == 'true';
 
-    $version_query = xtc_db_query("SELECT configuration_value FROM " . TABLE_CONFIGURATION . " WHERE configuration_key = '" . $this->name . "_VERSION'");
-    if (xtc_db_num_rows($version_query)) {
-      xtc_db_query("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '" . $this->version . "' WHERE configuration_key = '" . $this->name . "_VERSION'");
-    } elseif (defined($this->name . '_STATUS')) {
-      xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_VERSION', '" . $this->version . "', 6, 99, NULL, now())");
+        if (defined($this->name . '_VERSION') && $this->version != constant($this->name . '_VERSION')) {
+            xtc_db_query("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '" . $this->version . "' WHERE configuration_key = '" . $this->name . "_VERSION'");
+        } elseif (defined($this->name . '_STATUS') && !defined($this->name . '_VERSION')) {
+            xtc_db_query(
+              "INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_VERSION', '" . $this->version . "', 6, 99, NULL, now())"
+            );
+        }
     }
 
-  }
-
-  function process($file) {
-    //do nothing
-  }
-
-  function display() {
-    return array(
-      'text' => '<br>' . xtc_button(BUTTON_SAVE) . '&nbsp;' .
-        xtc_button_link(BUTTON_CANCEL, xtc_href_link(FILENAME_MODULE_EXPORT, 'set=' . $_GET['set'] . '&module=' . $this->code))
-    );
-  }
-
-  function check() {
-    if (!isset($this->_check)) {
-      $check_query = xtc_db_query("SELECT configuration_value FROM " . TABLE_CONFIGURATION . " WHERE configuration_key = '" . $this->name . "_STATUS'");
-      $this->_check = xtc_db_num_rows($check_query);
+    function process($file)
+    {
+        //do nothing
     }
-    return $this->_check;
-  }
 
-  function install() {
-    xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_STATUS', 'true', 6, 1, 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())");
-    xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_PROGRAMM_ID', '', 6, 2, NULL, now());");
-    xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_EVENT_ID', '', 6, 3, NULL, now());");
-    xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_RETARGETING', 'true', 6, 4, 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())");
-    xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_COOKIE_CONSENT_PURPOSE_ID', '', 6, 3, NULL, now());");
-    xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_VERSION', '" . $this->version . "', 6, 99, NULL, now())");
+    function display()
+    {
+        return array(
+          'text' => '<br>' . xtc_button(BUTTON_SAVE) . '&nbsp;' .
+            xtc_button_link(BUTTON_CANCEL, xtc_href_link(FILENAME_MODULE_EXPORT, 'set=' . $_GET['set'] . '&module=' . $this->code))
+        );
+    }
 
-    xtc_db_query("ALTER TABLE " . TABLE_CUSTOMERS . " ADD `mits_adcell_bid` VARCHAR(250) NULL DEFAULT NULL");
-  }
+    function check() {
+        if (!isset($this->_check)) {
+            if (defined($this->name . '_STATUS') && !defined('RUN_MODE_ADMIN')) {
+                $this->_check = true;
+            } else {
+                $check_query = xtc_db_query("SELECT configuration_value FROM " . TABLE_CONFIGURATION . " WHERE configuration_key = '" . $this->name . "_STATUS'");
+                $this->_check = xtc_db_num_rows($check_query);
+            }
+        }
+        return $this->_check;
+    }
 
-  function remove() {
-    xtc_db_query("DELETE FROM " . TABLE_CONFIGURATION . " WHERE configuration_key in ('" . implode("', '", $this->keys()) . "')");
-  }
+    function install()
+    {
+        xtc_db_query(
+          "INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_STATUS', 'true', 6, 1, 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())"
+        );
+        xtc_db_query(
+          "INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_PROGRAMM_ID', '', 6, 2, NULL, now());"
+        );
+        xtc_db_query(
+          "INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_EVENT_ID', '', 6, 3, NULL, now());"
+        );
+        xtc_db_query(
+          "INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_RETARGETING', 'true', 6, 4, 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())"
+        );
+        xtc_db_query(
+          "INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_COOKIE_CONSENT_PURPOSE_ID', '', 6, 3, NULL, now());"
+        );
+        xtc_db_query(
+          "INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('" . $this->name . "_VERSION', '" . $this->version . "', 6, 99, NULL, now())"
+        );
 
-  function keys() {
-    return array(
-      $this->name . '_STATUS',
-      $this->name . '_PROGRAMM_ID',
-      $this->name . '_EVENT_ID',
-      $this->name . '_RETARGETING',
-      $this->name . '_COOKIE_CONSENT_PURPOSE_ID'
-    );
-  }
+        xtc_db_query("ALTER TABLE " . TABLE_CUSTOMERS . " ADD `mits_adcell_bid` VARCHAR(250) NULL DEFAULT NULL");
+    }
+
+    function remove()
+    {
+        xtc_db_query("DELETE FROM " . TABLE_CONFIGURATION . " WHERE configuration_key in ('" . implode("', '", $this->keys()) . "')");
+        xtc_db_query("DELETE FROM " . TABLE_CONFIGURATION . " WHERE configuration_key LIKE '" . $this->name . "_%'");
+    }
+
+    function keys()
+    {
+        return array(
+          $this->name . '_STATUS',
+          $this->name . '_PROGRAMM_ID',
+          $this->name . '_EVENT_ID',
+          $this->name . '_RETARGETING',
+          $this->name . '_COOKIE_CONSENT_PURPOSE_ID'
+        );
+    }
 
 }
--- shoproot/includes/extra/application_top/application_top_end/85_mits_adcell_tracking.php
+++ shoproot/includes/extra/application_top/application_top_end/85_mits_adcell_tracking.php
@@ -16,22 +16,21 @@ if (defined('MODULE_MITS_ADCELL_TRACKING_STATUS') && strtolower(MODULE_MITS_ADCE
   && defined('MODULE_MITS_ADCELL_TRACKING_PROGRAMM_ID') && MODULE_MITS_ADCELL_TRACKING_PROGRAMM_ID != ''
   && defined('MODULE_MITS_ADCELL_TRACKING_EVENT_ID') && MODULE_MITS_ADCELL_TRACKING_EVENT_ID != ''
 ) {
-
-  if (isset($_GET['bid']) && !empty($_GET['bid']) && !isset($_SESSION['bid'])) {
-    $_SESSION['bid'] = xtc_db_prepare_input($_GET['bid']);
-    //$_COOKIE['_bid'] = xtc_db_prepare_input($_GET['bid']);
-  }
-
-  if (isset($_SESSION['customer_id'])) {
-    $adcell_bid_query = xtc_db_query("SELECT mits_adcell_bid FROM " . TABLE_CUSTOMERS . " WHERE customers_id = " . (int)$_SESSION['customer_id']);
-    $adcell_bid = xtc_db_fetch_array($adcell_bid_query);
-    if (isset($adcell_bid['mits_adcell_bid']) && !empty($adcell_bid['mits_adcell_bid'])) {
-      $_SESSION['bid'] = $adcell_bid['mits_adcell_bid'];
-      //$_COOKIE['_bid'] = $adcell_bid['mits_adcell_bid'];
-    } else {
-      if (isset($_SESSION['bid'])) {
-        xtc_db_query("UPDATE " . TABLE_CUSTOMERS . " SET mits_adcell_bid = '" . xtc_db_input($_SESSION['bid']) . "' WHERE customers_id = " . (int)$_SESSION['customer_id']);
-      }
+    if (isset($_GET['bid']) && !empty($_GET['bid']) && !isset($_SESSION['bid'])) {
+        $_SESSION['bid'] = xtc_db_prepare_input($_GET['bid']);
+        //$_COOKIE['_bid'] = xtc_db_prepare_input($_GET['bid']);
     }
-  }
+
+    if (isset($_SESSION['customer_id'])) {
+        $adcell_bid_query = xtc_db_query("SELECT mits_adcell_bid FROM " . TABLE_CUSTOMERS . " WHERE customers_id = " . (int)$_SESSION['customer_id']);
+        $adcell_bid = xtc_db_fetch_array($adcell_bid_query);
+        if (isset($adcell_bid['mits_adcell_bid']) && !empty($adcell_bid['mits_adcell_bid'])) {
+            $_SESSION['bid'] = $adcell_bid['mits_adcell_bid'];
+            //$_COOKIE['_bid'] = $adcell_bid['mits_adcell_bid'];
+        } else {
+            if (isset($_SESSION['bid'])) {
+                xtc_db_query("UPDATE " . TABLE_CUSTOMERS . " SET mits_adcell_bid = '" . xtc_db_input($_SESSION['bid']) . "' WHERE customers_id = " . (int)$_SESSION['customer_id']);
+            }
+        }
+    }
 }
